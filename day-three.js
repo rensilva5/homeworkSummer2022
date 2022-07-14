66a8d499559b57d571018e2123c40819c8566462
Renzo Silva <renzosilva@Renzos-MacBook-Air.local>
2022-07-14 10:40:23 -0400

    created homework folder

---
+++ day-three.js
@@ -0,0 +1,29 @@
+//--- Homework-1 -- add two numbers
+
+function addnumbers (num1, num2) {
+    return num1 + num2
+}
+console.log(addnumbers(4,8))
+
+//--- car Object // Do we need to come up with values?
+
+let car = {
+    model: ' ',
+    color: ' ',
+}
+console.log(car)
+car.year = ''
+console.log(car)
+
+console.log(car.model)
+console.log(car.color)
+console.log(car.year)
+
+//--- function first element in an array
+
+let arrayH = ['bread', 'butter', 'oil']
+let fruit = ['apples', 'bananas']
+function getElement (arr, i) {
+    return arr
+}
+console.log(getElement(fruit, 1))
