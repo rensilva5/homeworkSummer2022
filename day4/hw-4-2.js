f1e600021379277f9dea9eb4e2b61287f39ff28c
Renzo Silva <renzoasilva@gmail.com>
2022-07-15 09:07:11 -0400

    First push practice

---
+++ day4/hw-4-2.js
@@ -0,0 +1,9 @@
+//Create a function that returns the number of true values in an array.
+
+const testArray = [true, false, false, true, false, false, false, true, true, true, false]
+
+function test() {
+    return arr * true
+    
+}
+test()
---
+++ day4/hw-4-3.js
@@ -0,0 +1,10 @@
+// Create a function that converts a string to all uppercase letters and console.log the result (hint: string methods):
+// let str = “WHAT a TIME to BE ALIVE!”
+
+let word = “WHAT a TIME to BE ALIVE!”
+
+function UpperCase () {
+    //let word = “WHAT a TIME to BE ALIVE!”
+    return word.toLowerCase()
+}
+//console.log(UpperCase())
