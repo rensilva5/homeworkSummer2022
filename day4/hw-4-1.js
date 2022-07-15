7584b687bb83ec62693f3be97dce4269eb5b50ea
Renzo Silva <renzoasilva@gmail.com>
2022-07-15 12:55:22 -0400

    New update

--- day4/hw-4-1.js
+++ day4/hw-4-1.js
@@ -1,18 +1,18 @@
 // -- function assignGrade
 
-function assignGrade (score) {
-    if (score >= 9 && score <= 10) {
-        console.log("Awesome, you got an A")
-    } else if (score >= 8 && score < 9) {
-        console.log("B")
-    } else if (score >= 7 && score < 8) {
-        console.log("C")
-    } else if(score >= 6 && score < 7) {
-        console.log("D")
-    } else {
-        console.log("F")
-    }
+function assignGrade(score) {
+  if (score >= 9 && score <= 10) {
+    console.log("Awesome, you got an A");
+  } else if (score >= 8 && score < 9) {
+    console.log("B");
+  } else if (score >= 7 && score < 8) {
+    console.log("C");
+  } else if (score >= 6 && score < 7) {
+    console.log("D");
+  } else {
+    console.log("F");
+  }
 }
-//let grade = 
-console.log(assignGrade(9))  // - To check why undefined!!
+let grade = 7
+assignGrade(grade); // - To check why undefined!!
 //console.log(grade)
