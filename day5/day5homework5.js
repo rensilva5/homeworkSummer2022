4fa6bb0f4169cc43ba4eea8a90445922ad695311
Renzo Silva <renzoasilva@gmail.com>
2022-07-19 22:36:05 -0400

    Reversing an array

--- day5/day5homework5.js
+++ day5/day5homework5.js
@@ -1,7 +1,12 @@
 // - given a sentence reverse order: "the sky is blue" -> "euld si yks eht" */
 
-let sentence = "the sky is blue"
+// let sentence = "the sky is blue"
 
-function reverse() {
+function reverse(str) {
+    let intoArray = str.split("") 
+    let reverseArray = intoArray.reverse()
+    let reverseString = reverseArray.join("")
+    return reverseString
     
 }
+console.log(reverse("hello"));
